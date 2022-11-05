1705337c611bcf01d481c5fb55cc7365f7036af1
CarlosSuarezJS20 <69536882+CarlosSuarezJS20@users.noreply.github.com>
2022-11-05 19:18:21 +0000

    Improved Routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,13 +4,26 @@ import { ThemeProvider } from "@mui/material/styles";
 
 import theme from "./ui/theme";
 import { CssBaseline } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Header />
-      Hello
+      <BrowserRouter>
+        <CssBaseline />
+        <Header />
+        <Routes>
+          <Route path='/' element={<div>home</div>} />
+          <Route path='/services' element={<div>services</div>} />
+          <Route path='/customsoftware' element={<div>customsoftware</div>} />
+          <Route path='/mobileapps' element={<div>mobileapps</div>} />
+          <Route path='/websites' element={<div>websites</div>} />
+          <Route path='/revolution' element={<div>revolution</div>} />
+          <Route path='/about' element={<div>about</div>} />
+          <Route path='/contact' element={<div>contact</div>} />
+          <Route path='/estimate' element={<div>estimate</div>} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 };
--- src/components/ui/toolbar.tsx
+++ src/components/ui/toolbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ToolBar from "@mui/material/Toolbar";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
@@ -7,6 +7,8 @@ import Button from "@mui/material/Button";
 import { makeStyles } from "@mui/styles";
 import { styled } from "@mui/material/styles";
 import theme from "./theme";
+
+import { Link } from "react-router-dom";
 
 // import Typography from "@mui/material/Typography";
 
@@ -22,14 +24,21 @@ const useStyles = makeStyles(() => ({
     marginLeft: "auto",
     color: "white",
   },
+  tab: {
+    "&.MuiTab-root": {
+      ...theme.typography.tab,
+      minWidth: 10,
+      marginLeft: "25px",
+    },
+  },
+  logoContainer: {
+    "&.MuiButton-root": {
+      padding: 0,
+    },
+  },
 }));
 
 // Styling the tab, need to create a
-const StyledTab = styled(Tab)(() => ({
-  ...theme.typography.tab,
-  minWidth: 10,
-  marginLeft: "25px",
-}));
 
 const StyledButton = styled(Button)(() => ({
   ...theme.typography.estimateBtn,
@@ -48,10 +57,35 @@ const ToolBarCom: React.FC = () => {
     setValue(value);
   };
 
+  useEffect(() => {
+    let refreshValue: any;
+    if (window.location.pathname === "/" && value != 0) {
+      refreshValue = 0;
+    } else if (window.location.pathname === "/services" && value != 1) {
+      refreshValue = 1;
+    } else if (window.location.pathname === "/revolution" && value != 2) {
+      refreshValue = 2;
+    } else if (window.location.pathname === "/about" && value != 3) {
+      refreshValue = 3;
+    } else if (window.location.pathname === "/contact" && value != 4) {
+      refreshValue = 4;
+    }
+    setValue(refreshValue);
+  }, []);
+
   return (
     <ToolBar disableGutters>
       {/* <Typography variant='h3'>Arc Development</Typography> */}
-      <img src={logo} alt='company logo' className={classes.logo} />
+      <Button
+        component={Link}
+        to='/'
+        onClick={() => {
+          setValue(0);
+        }}
+        className={classes.logoContainer}>
+        <img src={logo} alt='company logo' className={classes.logo} />
+      </Button>
+
       <Tabs
         value={value}
         onChange={onChangeHandler}
@@ -60,11 +94,41 @@ const ToolBarCom: React.FC = () => {
         className={classes.tabsContainer}
         variant='scrollable'
         scrollButtons='auto'>
-        <StyledTab value={0} label='Home' />
-        <StyledTab value={1} label='Services' />
-        <StyledTab value={2} label=' The Revolution' />
-        <StyledTab value={3} label='About Us' />
-        <StyledTab value={4} label=' Contact Us' />
+        <Tab
+          component={Link}
+          to='/'
+          className={classes.tab}
+          value={0}
+          label='Home'
+        />
+        <Tab
+          component={Link}
+          to='/services'
+          className={classes.tab}
+          value={1}
+          label='Services'
+        />
+        <Tab
+          component={Link}
+          to='revolution'
+          className={classes.tab}
+          value={2}
+          label=' The Revolution'
+        />
+        <Tab
+          component={Link}
+          to='about'
+          className={classes.tab}
+          value={3}
+          label='About Us'
+        />
+        <Tab
+          component={Link}
+          to='contact'
+          className={classes.tab}
+          value={4}
+          label=' Contact Us'
+        />
       </Tabs>
       <StyledButton variant='contained' color='secondary'>
         Free Trial
